350ed771566faf2b43a5fd504074ffd2c42494e4
1303575952@qq.com <1303575952@qq.com>
2019-01-18 22:04:23 +0800

    chapter2

---
+++ chapter2/getinfo.cpp
@@ -0,0 +1,19 @@
+//
+// Created by FELIX on 2019/1/18.
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    int carrots;
+
+    cout << "How many carrots do you have?" << endl;
+    cin >> carrots;                // C++ input
+    cout << "Here are two more. ";
+    carrots = carrots + 2;
+// the next line concatenates output
+    cout << "Now you have " << carrots << " carrots." << endl;
+    return 0;
+}
---
+++ chapter2/myfirst.cpp
@@ -0,0 +1,13 @@
+//
+// Created by FELIX on 2019/1/18.
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+    return 0;
+}
