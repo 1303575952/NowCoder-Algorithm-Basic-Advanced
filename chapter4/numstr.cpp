a070a76a6f5d059e4bcd4204852feff9f429bd0d
1303575952@qq.com <1303575952@qq.com>
2019-01-19 15:22:12 +0800

    chapter4

---
+++ chapter4/numstr.cpp
@@ -0,0 +1,20 @@
+//
+// Created by FELIX on 2019/1/19.
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    cout << "What year was your house built?\n";
+    int year;
+    cin >> year;
+    cout << "What is its street address?\n";
+    char address[80];
+    cin.getline(address, 80);
+    cout << "Year built: " << year << endl;
+    cout << "Address: " << address << endl;
+    cout << "Done!\n";
+    // cin.get();
+    return 0;
+}
---
+++ chapter4/strtype4.cpp
@@ -0,0 +1,30 @@
+//
+// Created by FELIX on 2019/1/19.
+//
+
+#include <iostream>
+#include <string>               // make string class available
+#include <cstring>              // C-style string library
+
+int main() {
+    using namespace std;
+    char charr[20];
+    string str;
+
+    cout << "Length of string in charr before input: "
+         << strlen(charr) << endl;
+    cout << "Length of string in str before input: "
+         << str.size() << endl;
+    cout << "Enter a line of text:\n";
+    cin.getline(charr, 20);     // indicate maximum length
+    cout << "You entered: " << charr << endl;
+    cout << "Enter another line of text:\n";
+    getline(cin, str);          // cin now an argument; no length specifier
+    cout << "You entered: " << str << endl;
+    cout << "Length of string in charr after input: "
+         << strlen(charr) << endl;
+    cout << "Length of string in str after input: "
+         << str.size() << endl;
+
+    return 0;
+}
